9915d77345047170cd442760b282d8cd36f68ddf
HPro <prosserhamish@gmail.com>
2024-07-06 14:07:39 +1000

    load brew based on os

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
+switch (uname)
+    case Darwin
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    case Linux
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    case '*'
+        echo "Not mac or linux but a third option"
+end
+
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.dotnet/tools:$PATH"
 
@@ -19,13 +28,14 @@ function cat
 end
 
 # Ocaml
-eval "$(/opt/homebrew/bin/brew shellenv)"
-source /Users/hpro/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 eval $(opam config env)
 
 # Git
 alias gs="git status"
 alias lg="lazygit"
+
+alias hs="history | grep"
 
 starship init fish | source
 zoxide init --cmd cd fish | source
